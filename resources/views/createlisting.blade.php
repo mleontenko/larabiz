e5771f40ab3bf834269831dcbbfeb897707123b2
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-25 11:23:23 +0100

    createlisting - go back button

--- resources/views/createlisting.blade.php
+++ resources/views/createlisting.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
-            <div class="panel-heading">Create Listing</div>
+            <div class="panel-heading">Create Listing <a href="/dashboard" class="pull-right btn btn-default btn-xs">Go Back</a></div>
 
             <div class="panel-body">
                 {!! Form::open(['action' => 'ListingsController@store', 'method' => 'POST']) !!}
