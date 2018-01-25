e3de83e7045e4bc56f81ff8a1fc85addbaddaeb4
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-25 10:24:50 +0100

    add form

--- resources/views/createlisting.blade.php
+++ resources/views/createlisting.blade.php
@@ -7,7 +7,14 @@
             <div class="panel-heading">Create Listing</div>
 
             <div class="panel-body">
-                               
+                {!! Form::open(['action' => 'ListingsController@store', 'method' => 'POST']) !!}
+                    {{Form::bsText('name', '', ['placeholder' => 'Company Name'])}}
+                    {{Form::bsText('email', '', ['placeholder' => 'Contact Email'])}}
+                    {{Form::bsText('phone', '', ['placeholder' => 'Contact Phone'])}}
+                    {{Form::bsText('address', '', ['placeholder' => 'Business Address'])}}
+                    {{Form::bsText('bio', '', ['placeholder' => 'About This Business'])}}
+                    {{Form::bsSubmit('submit')}}
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
