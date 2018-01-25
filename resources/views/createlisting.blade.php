7dbf07697d67c03f75001c27cc79571c86958978
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-25 09:36:11 +0100

    add create template and formatting

---
+++ resources/views/createlisting.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Create Listing</div>
+
+            <div class="panel-body">
+                               
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
